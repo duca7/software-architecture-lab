92a12a2ca166f3ba797e9e771e552d583c57021c
duc <hohuynhduca7bh123@gmail.com>
2021-04-27 11:28:44 +0700

    phone site done

--- PhoneSite/PhoneSite/PhoneBUS.cs
+++ PhoneSite/PhoneSite/PhoneBUS.cs
@@ -7,5 +7,38 @@ namespace PhoneSite
 {
     public class PhoneBUS
     {
+        public List<Phone> GetAll()
+        {
+            List<Phone> phones = new PhoneDAO().SelectAll();
+            return phones;
+        }
+
+        public Phone GetDetails(int code)
+        {
+            Phone phone = new PhoneDAO().SelectByCode(code);
+            return phone;
+        }
+
+        public List<Phone> Search(String keyword)
+        {
+            List<Phone> phones = new PhoneDAO().SelectByKeyword(keyword);
+            return phones;
+        }
+        public bool AddNew(Phone newPhone)
+        {
+            bool resutl = new PhoneDAO().Insert(newPhone);
+            return resutl;
+        }
+
+        public bool Update(Phone newPhone)
+        {
+            bool resutl = new PhoneDAO().Update(newPhone);
+            return resutl;
+        }
+        public bool Delete(int code)
+        {
+            bool resutl = new PhoneDAO().Delete(code);
+            return resutl;
+        }
     }
 }
--- PhoneSite/PhoneSite/PhoneForm.aspx.cs
+++ PhoneSite/PhoneSite/PhoneForm.aspx.cs
@@ -11,7 +11,84 @@ namespace PhoneSite
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            if (!Page.IsPostBack)
+            {
+                List<Phone> phones = new PhoneBUS().GetAll();
+                gvPhones.DataSource = phones;
+                gvPhones.DataBind();
+            }
+        }
 
+        protected void gvPhones_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            int code = int.Parse(gvPhones.SelectedRow.Cells[1].Text.Trim());
+            Phone phone = new PhoneBUS().GetDetails(code);
+            if(phone != null)
+            {
+                txtCode.Text = phone.Code.ToString();
+                txtName.Text = phone.Name;
+                txtColor.Text = phone.Color;
+                txtPrice.Text = phone.Price.ToString();
+                txtQuantity.Text = phone.Quantity.ToString();
+            }
+        }
+
+        protected void btnSearch_Click(object sender, EventArgs e)
+        {
+            String keyword = txtKeyword.Text.Trim();
+            List<Phone> phones = new PhoneBUS().Search(keyword);
+            gvPhones.DataSource = phones;
+            gvPhones.DataBind();
+        }
+
+        protected void btnAdd_Click(object sender, EventArgs e)
+        {
+            Phone newPhone = new Phone()
+            {
+                
+                Name = txtName.Text.Trim(),
+                Color = txtColor.Text.Trim(),
+                Price = int.Parse(txtPrice.Text.Trim()),
+                Quantity = int.Parse(txtQuantity.Text.Trim())
+            };
+            bool result = new PhoneBUS().AddNew(newPhone);
+            if (result)
+            {
+                List<Phone> phones = new PhoneBUS().GetAll();
+                gvPhones.DataSource = phones;
+                gvPhones.DataBind();
+            }
+        }
+
+        protected void btnUpdate_Click(object sender, EventArgs e)
+        {
+            Phone newPhone = new Phone()
+            {
+                Code = int.Parse(txtCode.Text.Trim()),
+                Name = txtName.Text.Trim(),
+                Color = txtColor.Text.Trim(),
+                Price = int.Parse(txtPrice.Text.Trim()),
+                Quantity = int.Parse(txtQuantity.Text.Trim())
+            };
+            bool result = new PhoneBUS().Update(newPhone);
+            if (result)
+            {
+                List<Phone> phones = new PhoneBUS().GetAll();
+                gvPhones.DataSource = phones;
+                gvPhones.DataBind();
+            }
+        }
+
+        protected void btnDelete_Click(object sender, EventArgs e)
+        {
+            int code = int.Parse(txtCode.Text.Trim());
+            bool result = new PhoneBUS().Delete(code);
+            if (result) 
+            {
+                List<Phone> phones = new PhoneBUS().GetAll();
+                gvPhones.DataSource = phones;
+                gvPhones.DataBind();
+            }
         }
     }
 }
